1da478784d6b91bbe6364e6076a645c839926fef
Satyanarayana Chittidi <153839365+Satya3646@users.noreply.github.com>
2025-12-02 12:29:46 +0530

    Time: 159 ms (29.74%) | Memory: 215.4 MB (28.16%) - LeetSync

--- 3886-count-number-of-trapezoids-i/count-number-of-trapezoids-i.cpp
+++ 3886-count-number-of-trapezoids-i/count-number-of-trapezoids-i.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     int countTrapezoids(vector<vector<int>>& points) {
-        int mod = 1e9 + 7;
-        unordered_map <int, int> mp;
+        int mod = 1e9 + 7; // Modulo Value
+        unordered_map <int, int> mp; // Map to store number of points on same horizontal line
         for(vector <int> v : points)
             mp[v[1]]++;
+        // The size of map will be number of horizontal lines, and the values will be number of points on those lines
+        // We can pick any two lines, then pick any two points from one line and two from another line the 4 points will definitely form a trapezoid. Hence finding the total is the answer.
         long long ans = 0;
         long long edgeSum = 0;
+        // Clever combination instead of using two loops
         for(pair <int, int> p : mp)
         {
             long long edges = (1LL*p.second) * (p.second-1) / 2;
