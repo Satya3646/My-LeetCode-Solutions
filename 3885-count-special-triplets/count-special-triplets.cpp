f1c0aefbbbc5b9790cad464c476a80a78abb4a3e
Satyanarayana Chittidi <153839365+Satya3646@users.noreply.github.com>
2025-12-09 12:30:43 +0530

    Time: 911 ms (40.87%) | Memory: 428 MB (36.86%) - LeetSync

---
+++ 3885-count-special-triplets/count-special-triplets.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int specialTriplets(vector <int> &nums)
+    {
+        int n = nums.size();
+        int mod = 1e9+7;
+        unordered_map <int, int> l,r; // maps to keep left and right counts.
+        for(auto i : nums) // Initially rmap has whole array.
+            r[i]++;
+        // Handle the zeros beforehand.
+        int zeros = r[0];
+        r.erase(0);
+        long long ans = (1LL * zeros * (zeros-1) * (zeros-2)) / 6; // nC3 for zeros.
+        ans %= mod;
+        if(zeros == n)
+            return ans;
+        for(int i = 0; i < n; i++)
+        {
+            if(nums[i] == 0) // Skip the zeros.
+                continue;
+            r[nums[i]]--; // Remove current element(nums[j]) from right.
+            ans += l[2*nums[i]] * r[2*nums[i]];
+            ans %= mod;
+            l[nums[i]]++; // Add the current element to left before going to next iteration.
+        }
+        return ans;
+    }
+};
