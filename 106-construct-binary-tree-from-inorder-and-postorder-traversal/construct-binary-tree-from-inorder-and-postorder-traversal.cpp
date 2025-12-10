c8df5352edac2b8eb147449c5a43c953279ac4a7
Satyanarayana Chittidi <153839365+Satya3646@users.noreply.github.com>
2025-12-10 17:00:41 +0530

    Time: 6 ms (30.95%) | Memory: 27.6 MB (15.89%) - LeetSync

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -20,8 +20,8 @@ public:
         // iL to k-1 will be left subtree inorder.
         // k+1 to iR will be right subtree inorder.
         int leftSize = k-iL;
-        // pL to pL+leftSize-1 is left subtree preorder.
-        // pL+leftSize to pR-1 is right subtree preorder.
+        // pL to pL+leftSize-1 is left subtree postorder.
+        // pL+leftSize to pR-1 is right subtree postorder.
         root->left = helper(postorder, pL, pL+leftSize-1, inorder, iL, k-1, rootLookup);
         root->right = helper(postorder, pL+leftSize, pR-1, inorder, k+1, iR, rootLookup);
         return root;
