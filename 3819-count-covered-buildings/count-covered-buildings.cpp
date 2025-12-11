c5ec58a8acb21a07fa6142ee75feaf0aef6ce804
Satyanarayana Chittidi <153839365+Satya3646@users.noreply.github.com>
2025-12-11 12:09:04 +0530

    Time: 372 ms (60.77%) | Memory: 362.3 MB (57.46%) - LeetSync

--- 3819-count-covered-buildings/count-covered-buildings.cpp
+++ 3819-count-covered-buildings/count-covered-buildings.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     int countCoveredBuildings(int n, vector<vector<int>>& buildings) {
-        unordered_map <int, pair <int, int>> horizontal, vertical;
-        for(vector <int> v : buildings)
+        vector <int> minX(n+1, INT_MAX), // min x coordinate for a given y coordinate.
+                     maxX(n+1, INT_MIN), // max x coordinate for a given y coordinate.
+                     minY(n+1, INT_MAX), // min y coordinate for a given x coordinate.
+                     maxY(n+1, INT_MIN); // max y coordinate for a given x coordinate.
+        for(vector <int> v : buildings) // Update the x and y limits.
         {
             int x = v[0];
             int y = v[1];
-            if(horizontal.find(y) != horizontal.end())
-                horizontal[y] = {min(horizontal[y].first, x), max(horizontal[y].second, x)};
-            else
-                horizontal[y] = {x, x};
-            if(vertical.find(x) != vertical.end())
-                vertical[x] = {min(vertical[x].first, y), max(vertical[x].second, y)};
-            else
-                vertical[x] = {y, y};
+            minX[y] = min(minX[y], v[0]);
+            maxX[y] = max(maxX[y], v[0]);
+            minY[x] = min(minY[x], v[1]);
+            maxY[x] = max(maxY[x], v[1]);
         }
         int ans = 0;
         for(vector <int> v : buildings)
         {
             int x = v[0];
             int y = v[1];
-            if(vertical[x].first < y && y < vertical[x].second &&
-               horizontal[y].first < x && x < horizontal[y].second)
+            // If the current point is within both x and y limits then its covered.
+            if(minX[y] < x && x < maxX[y] &&
+               minY[x] < y && y < maxY[x])
                 ans++;
         }
         return ans;
