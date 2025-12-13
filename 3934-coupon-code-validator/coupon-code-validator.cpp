fd19dbe41f495850a956623e81562d8c8729766d
Satyanarayana Chittidi <153839365+Satya3646@users.noreply.github.com>
2025-12-13 12:13:15 +0530

    Time: 2 ms (86.27%) | Memory: 68.4 MB (81.54%) - LeetSync

--- 3934-coupon-code-validator/coupon-code-validator.cpp
+++ 3934-coupon-code-validator/coupon-code-validator.cpp
@@ -10,34 +10,36 @@ public:
         return true;
     }
 
-    bool validBusinessLine(string &businessLine)
-    {
-        if(businessLine != "electronics" &&
-           businessLine != "grocery" &&
-           businessLine != "pharmacy" &&
-           businessLine != "restaurant")
-            return false;
-        return true;
-    }
-
     vector<string> validateCoupons(vector <string> &code, vector <string> &businessLine, vector <bool> &isActive)
     {
         int n = code.size();
-        vector <pair <string, string>> validCoupons;
+        vector <string> bkt1, bkt2, bkt3, bkt4;
         for(int i = 0; i < n; i++)
         {
             string bl = businessLine[i];
             if(!validCode(code[i]))
                 continue;
-            if(!validBusinessLine(bl))
-                continue;
             if(isActive[i])
-                validCoupons.push_back({bl, code[i]});
+            {
+                if(bl == "electronics")
+                    bkt1.push_back(code[i]);
+                else if(bl == "grocery")
+                    bkt2.push_back(code[i]);
+                else if(bl == "pharmacy")
+                    bkt3.push_back(code[i]);
+                else if(bl == "restaurant")
+                    bkt4.push_back(code[i]);
+            }
         }
-        sort(validCoupons.begin(), validCoupons.end());
-        vector <string> f;
-        for(pair <string, string> &p : validCoupons)
-            f.push_back(p.second);
-        return f;
+        sort(bkt1.begin(), bkt1.end());
+        sort(bkt2.begin(), bkt2.end());
+        sort(bkt3.begin(), bkt3.end());
+        sort(bkt4.begin(), bkt4.end());
+        vector <string> validCoupons;
+        validCoupons.insert(validCoupons.end(), bkt1.begin(), bkt1.end());
+        validCoupons.insert(validCoupons.end(), bkt2.begin(), bkt2.end());
+        validCoupons.insert(validCoupons.end(), bkt3.begin(), bkt3.end());
+        validCoupons.insert(validCoupons.end(), bkt4.begin(), bkt4.end());
+        return validCoupons;
     }
 };
