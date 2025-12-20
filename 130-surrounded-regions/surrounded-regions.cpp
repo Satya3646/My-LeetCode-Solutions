1ac777f84fedcdd8bafd8b01904a3398e3f85936
Satyanarayana Chittidi <153839365+Satya3646@users.noreply.github.com>
2025-12-20 21:33:42 +0530

    Time: 0 ms (100.00%) | Memory: 14.7 MB (35.85%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -9,7 +9,15 @@ public:
         int n = board[0].size();
         queue <pair <int, int>> q;
 
-        for(int i = 0; i < m; i++)
+        /*
+        1) Put all the zeros on the boundary into the queue while marking them as visited.
+        2) Since the board is a character board we can just use a sentinel character (#) to mark visited without needing an explicit visited array.
+        3) Now we can do BFS traversal from these boundary zeros marking all their adjacent zeors as '#'.
+        4) At the end the remaining zeros are the surrounded zeros which we will mark as 'X' and all the '#' are zeros, restore the board by changing '#' to 'O'.
+        */
+
+        // Put all the zeros on left and right boundary into queue and mark them as visited using a different character (#).
+        for(int i = 0; i < m; i++) 
         {
             if(board[i][0] == 'O')
                 board[i][0] = '#',
@@ -19,6 +27,7 @@ public:
                 q.push({i, n-1});
         }
 
+        // Put all the zeros on top and bottom boundary into queue and mark them as visited using a different character (#).
         for(int i = 0; i < n; i++)
         {
             if(board[0][i] == 'O')  
@@ -34,19 +43,20 @@ public:
             int qs = q.size();
             for(int i = 0; i < qs; i++)
             {
-                auto [x, y] = q.front();
+                auto [x, y] = q.front(); // Unpack the coordinates.
                 q.pop();
-                for(int j = 0; j < 4; j++)
+                for(int j = 0; j < 4; j++) // Iterate the adjacents.
                 {
                     int nx = x + dx[j];
                     int ny = y + dy[j];
-                    if(0 <= nx && nx < m && 0 <= ny && ny < n && board[nx][ny] == 'O')
+                    if(0 <= nx && nx < m && 0 <= ny && ny < n && board[nx][ny] == 'O') // if this is zero that is adjacent to a boundary zero the marks this a visited using '#'.
                         board[nx][ny] = '#', 
                         q.push({nx, ny});
                 }
             }
         }
 
+        // After the traversal completed all the zeros left are the surrounded zeros (not connected to any boundary), so we can marks the as 'X', at the same time we can restore the boundary connected zeros we marked earlier.
         for(int i = 0; i < m; i++)
         {
             for(int j = 0; j < n; j++)
