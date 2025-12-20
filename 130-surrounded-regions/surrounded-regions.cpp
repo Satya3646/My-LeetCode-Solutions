385720af303be133fa93fe2b2da498987db5991d
Satyanarayana Chittidi <153839365+Satya3646@users.noreply.github.com>
2025-12-20 21:24:25 +0530

    Time: 0 ms (100.00%) | Memory: 14.4 MB (67.04%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -7,23 +7,22 @@ public:
     {
         int m = board.size();
         int n = board[0].size();
-        vector <vector <bool>> visited(m, vector <bool> (n, false));
         queue <pair <int, int>> q;
 
         for(int i = 0; i < m; i++)
         {
             if(board[i][0] == 'O')
-                q.push({i, 0});
+                board[i][0] = '#', q.push({i, 0});
             if(board[i][n-1] == 'O')
-                q.push({i, n-1});
+                board[i][n-1] = '#', q.push({i, n-1});
         }
 
         for(int i = 0; i < n; i++)
         {
             if(board[0][i] == 'O')  
-                q.push({0, i});
+                board[0][i] = '#', q.push({0, i});
             if(board[m-1][i] == 'O')
-                q.push({m-1, i});
+                board[m-1][i] = '#', q.push({m-1, i});
         }
 
         while(!q.empty())
@@ -33,20 +32,25 @@ public:
             {
                 auto [x, y] = q.front();
                 q.pop();
-                visited[x][y] = true;
                 for(int j = 0; j < 4; j++)
                 {
                     int nx = x + dx[j];
                     int ny = y + dy[j];
-                    if(0 <= nx && nx < m && 0 <= ny && ny < n && board[nx][ny] == 'O' && !visited[nx][ny])
-                        q.push({nx, ny});
+                    if(0 <= nx && nx < m && 0 <= ny && ny < n && board[nx][ny] == 'O')
+                        board[nx][ny] = '#', q.push({nx, ny});
                 }
             }
         }
 
         for(int i = 0; i < m; i++)
+        {
             for(int j = 0; j < n; j++)
-                if(!visited[i][j])
+            {
+                if(board[i][j] == 'O')
                     board[i][j] = 'X';
+                else if(board[i][j] == '#')
+                    board[i][j] = 'O';
+            }
+        }
     }
 };
