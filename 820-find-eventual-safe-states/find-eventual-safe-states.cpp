d137e5c9ca0f6595e851150bde58075b289e0822
Satyanarayana Chittidi <153839365+Satya3646@users.noreply.github.com>
2025-12-24 17:26:59 +0530

    Time: 48 ms (49.52%) | Memory: 65.8 MB (55.39%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -2,23 +2,21 @@ class Solution {
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n = graph.size();
-        vector <vector <int>> reverseGraph(n);
-        vector <int> indegree(n);
+        vector <vector <int>> reverseGraph(n); // reversed edges.
+        vector <int> indegree(n); // indegree of nodes in reversed graph.
+        queue <int> q;
+
         for(int i = 0; i < n; i++)
         {
-            indegree[i] = graph[i].size();
-            for(int node : graph[i])
-            {
+            indegree[i] = graph[i].size(); // number of nodes pointing to 'i'.
+            if(!indegree[i])
+                q.push(i);
+            for(int node : graph[i]) // take nodes in graph[i] (those coming out of i) and point them to i (towards i).
                 reverseGraph[node].push_back(i);
-            }
         }
 
         vector <int> safeNodes;
         safeNodes.reserve(n);
-        queue <int> q;
-        for(int i = 0; i < n; i++)
-            if(!indegree[i])
-                q.push(i);
         
         while(!q.empty())
         {
@@ -26,10 +24,8 @@ public:
             q.pop();
             safeNodes.push_back(node);
             for(int i : reverseGraph[node])
-            {
                 if(!--indegree[i])
                     q.push(i);
-            }
         }
 
         sort(safeNodes.begin(), safeNodes.end());
