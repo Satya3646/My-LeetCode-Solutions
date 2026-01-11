dd2cc8453b911d880ff58dc1c4442fc11498cd59
Satyanarayana Chittidi <153839365+Satya3646@users.noreply.github.com>
2026-01-11 17:36:31 +0530

    Time: 11 ms (33.61%) | Memory: 22 MB (10.78%) - LeetSync

---
+++ 85-maximal-rectangle/maximal-rectangle.cpp
@@ -0,0 +1,64 @@
+class Solution {
+public:
+    void NSE(vector <int> &arr, vector <int> &nse)
+    {
+        stack <int> stk;
+        int n = arr.size();
+        for(int i = n-1; i >= 0; i--)
+        {
+            // pop as long as the top is not less that current
+            while(!stk.empty() && arr[stk.top()] >= arr[i])
+                stk.pop();
+            if(!stk.empty())
+                nse[i] = stk.top();
+            stk.push(i);
+        }
+    }
+
+    void PSE(vector <int> &arr, vector <int> &pse)
+    {
+        stack <int> stk;
+        int n = arr.size();
+        for(int i = 0; i < n; i++)
+        {
+            // pop as long as the top is not less that current
+            while(!stk.empty() && arr[stk.top()] >= arr[i])
+                stk.pop();
+            if(!stk.empty())
+                pse[i] = stk.top();
+            stk.push(i);
+        }
+    }
+
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+        vector <int> nse(n, n);
+        vector <int> pse(n, -1);
+        NSE(heights, nse);
+        PSE(heights, pse);
+        int maxRect = 0;
+        for(int i = 0; i < n; i++)
+            maxRect = max(maxRect, (nse[i] - pse[i] - 1) * heights[i]);
+        return maxRect;
+    }
+
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        vector <vector <int>> prefix(n, vector <int> (m, 0));
+        for(int i = 0; i < m; i++)
+            prefix[0][i] = matrix[0][i] - '0';
+        for(int i = 1; i < n; i++)
+        {
+            for(int j = 0; j < m; j++)
+            {
+                if(matrix[i][j] == '1')
+                    prefix[i][j] = prefix[i-1][j] + matrix[i][j] - '0';
+            }
+        }
+        int maxRect = 0;
+        for(int i = 0; i < n; i++)
+            maxRect = max(maxRect, largestRectangleArea(prefix[i]));
+        return maxRect;
+    }
+};
