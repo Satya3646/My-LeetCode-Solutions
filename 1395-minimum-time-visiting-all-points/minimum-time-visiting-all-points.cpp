84b4790b96693c2b03048fb7ff5b388f33bb363d
Satyanarayana Chittidi <153839365+Satya3646@users.noreply.github.com>
2026-01-12 09:44:50 +0530

    Time: 0 ms (100.00%) | Memory: 13.8 MB (61.26%) - LeetSync

--- 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.cpp
+++ 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.cpp
@@ -5,10 +5,10 @@ public:
         int totalTime = 0;
         for(int i = 1; i < n; i++)
         {
-            int dx = abs(points[i][0] - points[i-1][0]);
-            int dy = abs(points[i][1] - points[i-1][1]);
-            int time = min(dx, dy);
-            time += abs(dx - dy);
+            int dx = abs(points[i][0] - points[i-1][0]); // x-diff
+            int dy = abs(points[i][1] - points[i-1][1]); // y-diff
+            int time = min(dx, dy); // diagonal movement can be done min(dx, dy) times.
+            time += abs(dx - dy); // final vertical/horizontal movement to account for extra dx or dy.
             totalTime += time;
         }
         return totalTime;
