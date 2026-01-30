6016b5debac4ac35d2fc8ca4cd75098e7f3e82ea
Satyanarayana Chittidi <153839365+Satya3646@users.noreply.github.com>
2026-01-30 17:55:47 +0530

    Time: 15 ms (72.30%) | Memory: 36.7 MB (34.29%) - LeetSync

--- 1306-minimum-absolute-difference/minimum-absolute-difference.cpp
+++ 1306-minimum-absolute-difference/minimum-absolute-difference.cpp
@@ -3,11 +3,17 @@ public:
     vector<vector<int>> minimumAbsDifference(vector<int>& arr)
     {
         int n = arr.size();
+
+        // The difference between adjacent elements is minimized in sorted order.
         sort(arr.begin(), arr.end());
         vector <vector <int>> ans;
         int minDiff = INT_MAX;
+
+        // The minimum difference first.
         for(int i = 1; i < n; i++)
             minDiff = min(minDiff, arr[i] - arr[i-1]);
+
+        // Find the pairs of elements with minimum difference.
         for(int i = 1; i < n; i++)
             if(arr[i] - arr[i-1] == minDiff)
                 ans.push_back({arr[i-1], arr[i]});
